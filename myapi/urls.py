2be10de2f5816abd3a7cb6c330de0f18716ae08c
Ahmed Alaa <ahmedjoey99@gmail.com>
2022-02-09 03:19:15 +0200

    Another commit

--- myapi/urls.py
+++ myapi/urls.py
@@ -2,11 +2,13 @@ from django.urls import path,include
 from .views import noster
 from rest_framework import routers
 from student_register.models import Student
-from .views import Studentlist
+from .views import Studentlist, deletestudent
+
 router = routers.DefaultRouter()
 router.register(r'student',Studentlist)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('delete/<id>', deletestudent),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- myapi/views.py
+++ myapi/views.py
@@ -4,7 +4,17 @@ from student_register.models import Student
 from rest_framework import viewsets
 from .serializers import StudentSerializer
 from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+@api_view(['POST'])
+def deletestudent (request,pk):
+    student = Student.objects.get(pk=id)
+    student.delete()
+    return Response({'message': '{} student was deleted successfully!'.format(student.fullname)})
+
 
 def noster (request):
     students = Student.objects.all()
